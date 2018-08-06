4ce93f1184e8ed389192e1df30d24f05137482a5
Maxim Nechaev <4max.nechaev@gmail.com>
2018-08-06 16:42:22 +0000

    Function that formats the data into readable form

---
+++ getAndPrintHTML.js
@@ -0,0 +1,21 @@
+function getAndPrintHTML () {
+  var https = require('https');
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step2.html'
+  };
+
+  var arr = [];
+
+  https.get(requestOptions, function(response) {
+    response.setEncoding('utf8');
+    response.on('data', function(data) {
+      for (var i = 0; i < data.length; i++) {
+        arr.push(arr[i]);
+      }
+      console.log(data);
+    });
+  });
+}
+
+getAndPrintHTML ();
