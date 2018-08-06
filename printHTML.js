4dc0c831efc37106d52aec836221f306bd726b69
Maxim Nechaev <4max.nechaev@gmail.com>
2018-08-06 18:31:32 +0000

    Storing HTML in a temporary text file added

--- printHTML.js
+++ printHTML.js
@@ -1,5 +1,8 @@
-function printHTML () {
+function printHTML() {
   var https = require('https');
+  var fs = require("fs");
+  var filePath = "/tmp/storedPage.txt";
+  // var fileData = "Testing asynchronous file write.\n";
   var url = process.argv[2];
   var arr = [];
 
@@ -11,8 +14,7 @@ function printHTML () {
     path: "/" + arr[0].slice(1, arr[0].length).join('/')
   };
 
-console.log(arr);
-console.log(requestOptions);
+  console.log(requestOptions);
   var arr2 = [];
   https.get(requestOptions, function(response) {
     response.setEncoding('utf8');
@@ -20,9 +22,14 @@ console.log(requestOptions);
       for (var i = 0; i < data.length; i++) {
         arr2.push(arr2[i]);
       }
-      console.log(data);
+      fs.writeFile(filePath, data, function(err) {
+        if (err) {
+          throw err;
+        }
+      });
+      console.log("The file is stored at", filePath);
     });
   });
 }
 
-printHTML ();
+printHTML();
